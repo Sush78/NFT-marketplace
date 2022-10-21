460003350c662e314a7929c94bef6b989c3f5d2c
Sushant Kumar <sushantkumar@Sushants-MacBook-Air-2.local>
2022-10-21 15:38:44 +0100

    setup NFT smart contract

--- smart_contract/contracts/NFT.sol
+++ smart_contract/contracts/NFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract NFT is ERC721URIStorage {
     uint public tokenCount;
 
-    constructor() ERC721("Skoin NFT", "SkOIN"){}
+    constructor() ERC721("Skoin NFT", "SKOIN"){}
 
     function mint(string memory _tokenURI) external returns(uint) {
         tokenCount ++;
