67ed2d99b4d2dce267edbdfe93c7a5a417b5ab97
Sushant Kumar <sushantkumar@Sushants-MacBook-Air-2.local>
2022-10-21 15:38:20 +0100

    setup NFT smart contract

--- smart_contract/contracts/NFT.sol
+++ smart_contract/contracts/NFT.sol
@@ -1,6 +1,17 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract NFT {
-    
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage {
+    uint public tokenCount;
+
+    constructor() ERC721("Skoin NFT", "SkOIN"){}
+
+    function mint(string memory _tokenURI) external returns(uint) {
+        tokenCount ++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return(tokenCount);
+    }
 }
